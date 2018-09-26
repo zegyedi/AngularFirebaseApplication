65e83973ff255a257f0cd1c53e6eca8ff20bc588
Zoltan Egyedi <egyedi.zoltan@gmail.com>
2018-09-26 17:22:08 +0200

    add channel model

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { DomSanitizer } from '@angular/platform-browser';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
 import { ChatUser } from '../../Models/user';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Messages } from '../../Models/messages';
 import { Observable } from 'rxjs';
+import { Channel } from '../../models/channel';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -21,36 +23,50 @@ export class ChatComponent implements OnInit {
 
   testUser: Observable<ChatUser>;
 
+  channels: Channel[] = [{ name: 'main' }, { name: 'test2' }, { name: 'test3' }];
+
   messageText = '';
   searchText = '';
 
-  constructor(private auth: AuthService, private sec: DomSanitizer, private fireDb: AngularFireDatabase) {
+  constructor(private auth: AuthService,
+    private sec: DomSanitizer,
+    private fireDb: AngularFireDatabase,
+    private route: ActivatedRoute) {
+
     this.userContext = this.fireDb.list('/users');
     this.messageContext = this.fireDb.list('/messages');
-
   }
 
   ngOnInit() {
     this.auth.user.pipe(
-      map(data => new ChatUser(data.uid, data.displayName, data.email, data.photoURL)))
+      map(data => new ChatUser(data.uid, data.displayName, data.email, data.photoURL)),
+      )
       .subscribe(user => {
         this.user = user;
         //  this.userContext.push( this.user);
         console.log(this.user);
       });
 
-    this.messages = this.messageContext.valueChanges();
+    this.messages = this.messageContext.valueChanges().pipe(
+      map(items => items.filter(item => item.channelName.indexOf(this.getRoute()) > -1)
+      ));
+
+
+  }
+
+  getRoute(): string {
+    return  this.route.snapshot.paramMap.get('name');
   }
 
   sendMessage() {
-    this.messageContext.push(new Messages(this.user.displayName, this.user.photo_source, this.messageText));
+    this.messageContext.push(new Messages(this.user.displayName, this.user.photo_source, this.messageText, this.getRoute()));
     this.messageText = '';
   }
 
   searchInMessages() {
     this.messages = this.messages.pipe(
       map(items => items.filter(item => item.text.indexOf(this.searchText) > -1)
-    ));
+      ));
   }
 
   searchClear() {
