a8bb32f7ec6dbda361af39da2f60a6dd05b3df36
Zoltan Egyedi <egyedi.zoltan@gmail.com>
2018-09-26 09:45:04 +0200

    remove conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCzGEF6yLJ-kOJBSZxx1OBQXLLVkqSW6S0',
-    authDomain: 'angularchatapplication-fdf86.firebaseapp.com',
-    databaseURL: 'https://angularchatapplication-fdf86.firebaseio.com',
-    projectId: 'angularchatapplication-fdf86',
-    storageBucket: 'angularchatapplication-fdf86.appspot.com',
-    messagingSenderId: '1009033074265'
+ 
   }
 };
